d62cff58e56d12ef12ecf7fbe7dedddb401a2ebe
mattszczp <mattszczp@gmail.com>
2022-07-29 23:59:49 +0200

    gofmt

--- main.go
+++ main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
+	"net/http"
 	"strings"
-    "net/http"
 
 	"github.com/gofiber/fiber/v2"
-    "github.com/gofiber/template/html"
-    "github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/template/html"
 )
 
 func main() {
-    engine := html.New("./views", ".html")
+	engine := html.New("./views", ".html")
 
-    app := fiber.New(fiber.Config{
-        Views: engine,
-    })
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
-    app.Use("/static", filesystem.New(filesystem.Config{
-        Root: http.Dir("./static"),
-        Browse: true,
-    }))
+	app.Use("/static", filesystem.New(filesystem.Config{
+		Root:   http.Dir("./static"),
+		Browse: true,
+	}))
 
-//    app.Static("/", "./static")
+	//    app.Static("/", "./static")
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.Render("index", fiber.Map{}, "layouts/main")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{}, "layouts/main")
+	})
 
-    app.Post("/evolution", func(c *fiber.Ctx) error {
-        chains := GetEvolutionChain(strings.ToLower(c.FormValue("pokemon")))
+	app.Post("/evolution", func(c *fiber.Ctx) error {
+		chains := GetEvolutionChain(strings.ToLower(c.FormValue("pokemon")))
 
-        if len(chains) == 0 {
-            return c.SendString("This pokemon doesn't have any evolution")
-        }
+		if len(chains) == 0 {
+			return c.SendString("This pokemon doesn't have any evolution")
+		}
 
-        return c.Render("chain", fiber.Map{
-            "Pokemon": c.FormValue("pokemon"),
-            "Chains": chains,
-        }, "layouts/main")
-    })
+		return c.Render("chain", fiber.Map{
+			"Pokemon": c.FormValue("pokemon"),
+			"Chains":  chains,
+		}, "layouts/main")
+	})
 
-    app.Listen(":3000")
+	app.Listen(":3000")
 }
--- pokemon.go
+++ pokemon.go
@@ -8,39 +8,39 @@ import (
 )
 
 type EvolutionChain struct {
-    Pokemons []string
+	Pokemons []string
 }
 
 //TODO: Replace panicking with returning HTTP errors
-func GetEvolutionChain(pokemon_name string)([]EvolutionChain) {
-    pokemon, err := pokeapi.PokemonSpecies(pokemon_name)
-    if err != nil {
-        log.Fatalf("Couldn't find a pokemon named: %s", pokemon_name)
-    }
+func GetEvolutionChain(pokemon_name string) []EvolutionChain {
+	pokemon, err := pokeapi.PokemonSpecies(pokemon_name)
+	if err != nil {
+		log.Fatalf("Couldn't find a pokemon named: %s", pokemon_name)
+	}
 
-    chain := pokemon.EvolutionChain.URL
-    chain_id := strings.Split(chain, "/")[6]
+	chain := pokemon.EvolutionChain.URL
+	chain_id := strings.Split(chain, "/")[6]
 
-    evo_chain, err := pokeapi.EvolutionChain(chain_id)
+	evo_chain, err := pokeapi.EvolutionChain(chain_id)
 
-    if err != nil {
-        log.Fatalf("Couldn't find th evolution chain for %s", pokemon_name)
-    }
+	if err != nil {
+		log.Fatalf("Couldn't find th evolution chain for %s", pokemon_name)
+	}
 
-    var chains []EvolutionChain
+	var chains []EvolutionChain
 
-    for _, evolution := range evo_chain.Chain.EvolvesTo {
+	for _, evolution := range evo_chain.Chain.EvolvesTo {
 
-        var chain EvolutionChain
+		var chain EvolutionChain
 
-        chain.Pokemons = append(chain.Pokemons, evo_chain.Chain.Species.Name, evolution.Species.Name)
+		chain.Pokemons = append(chain.Pokemons, evo_chain.Chain.Species.Name, evolution.Species.Name)
 
-        if len(evolution.EvolvesTo) > 0 {
-            chain.Pokemons = append(chain.Pokemons, evolution.EvolvesTo[0].Species.Name)
-        }
+		if len(evolution.EvolvesTo) > 0 {
+			chain.Pokemons = append(chain.Pokemons, evolution.EvolvesTo[0].Species.Name)
+		}
 
-        chains = append(chains, chain)
-    }
+		chains = append(chains, chain)
+	}
 
-    return chains
+	return chains
 }
