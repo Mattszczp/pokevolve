bc5887b8976ba54513896404837a2c09a6fa403e
Mateusz Szczepański <mattszczp@gmail.com>
2022-07-29 10:35:47 +0200

    pritn evolution chain

--- main.go
+++ main.go
@@ -4,19 +4,39 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
+	"strings"
 
 	"github.com/mtslzr/pokeapi-go"
 )
 
 func main() {
+    if len(os.Args) < 2 {
+        log.Fatalf("You need to provide a pokemon")
+    }
     pokemon_name := os.Args[1]
-    pokemon, err := pokeapi.Pokemon(pokemon_name)
+    pokemon, err := pokeapi.PokemonSpecies(pokemon_name)
     if err != nil {
         log.Fatalf("Couldn't find a pokemon named: %s", pokemon_name)
     }
 
-    pokemon_id := strconv.Itoa(pokemon.ID)
+    chain := pokemon.EvolutionChain.URL
+    chain_id := strings.Split(chain, "/")[6]
 
-    fmt.Println("The id of", pokemon_name , "is:", pokemon_id)
+    evo_chain, err := pokeapi.EvolutionChain(chain_id)
+
+    if err != nil {
+        log.Fatalf("Couldn't find th evolution chain for %s", pokemon_name)
+    }
+
+
+    for _, evolution := range evo_chain.Chain.EvolvesTo {
+
+        if len(evolution.EvolvesTo) > 0 {
+            fmt.Println(evo_chain.Chain.Species.Name,"->",evolution.Species.Name, "->", evolution.EvolvesTo[0].Species.Name)
+            break
+        }
+        fmt.Println(evo_chain.Chain.Species.Name,"->",evolution.Species.Name)
+    }
+
+
 }
